5f946669535db22d3376a0e88fe75d18e7819479
Oleg Shuruev <shuruev@gmail.com>
2014-03-06 12:21:46 -0800

    Added MySQL support.

---
+++ Lean.Database/DbHelper/MySql/MySqlDbHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace Lean.Database
+{
+	/// <summary>
+	/// Provides access to the MySQL database.
+	/// </summary>
+	public class MySqlDbHelper : DbHelper
+	{
+		/// <summary>
+		/// Initializes a new instance.
+		/// </summary>
+		public MySqlDbHelper(string connectionString, TimeSpan commandTimeout)
+			: base(connectionString, commandTimeout)
+		{
+		}
+
+		#region Connection
+
+		/// <summary>
+		/// Creates new connection.
+		/// </summary>
+		protected override DbConnection CreateConnection()
+		{
+			return new MySqlConnection(m_connectionString);
+		}
+
+		#endregion
+	}
+}
